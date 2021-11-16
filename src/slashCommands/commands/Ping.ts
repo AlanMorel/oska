659cca41f5e14010db1f960ae94afdc78ef47d1d
Alan Morel <alan@alanmorel.com>
2021-11-16 16:22:57 -0500

    Test embeds

--- src/slashCommands/commands/Ping.ts
+++ src/slashCommands/commands/Ping.ts
@@ -1,13 +1,22 @@
 import { SlashCommand } from "@/slashCommands/SlashCommand";
-import { BaseCommandInteraction, Client } from "discord.js";
+import { BaseCommandInteraction, Client, MessageEmbed } from "discord.js";
 
 export const Ping: SlashCommand = {
     name: "ping",
     description: "Returns websocket ping",
     type: "CHAT_INPUT",
     run: async (client: Client, interaction: BaseCommandInteraction) => {
+        const pembed = new MessageEmbed()
+            .setColor("#2F3136")
+            .setTitle("Client Ping")
+            .addField("**Latency**", `\`${Date.now() - interaction.createdTimestamp}ms\``)
+            .addField("**API Latency**", `\`${Math.round(client.ws.ping)}ms\``)
+            .setTimestamp()
+            .setFooter(`${interaction.user.username}`, interaction.user.avatarURL() ?? undefined);
+
         await interaction.followUp({
-            content: `${client.ws.ping}ms!`
+            content: `${client.ws.ping}ms!`,
+            embeds: [pembed]
         });
     }
 };

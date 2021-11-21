8231496bd8e1d3a85cc67e5835f4380db5d50bfa
Alan Morel <alan@alanmorel.com>
2021-11-21 18:16:13 -0500

    Give bot proper intent flags

--- src/Bot.ts
+++ src/Bot.ts
@@ -8,7 +8,12 @@ import { Client, Intents } from "discord.js";
 initializeCaches();
 
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES
+    ]
 });
 
 interactionCreate(client);

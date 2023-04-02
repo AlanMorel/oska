59e642c62a2c5fd6aee41789a4f81d2c0e40dcdb
Alan Morel <alan@alanmorel.com>
2023-04-02 16:35:37 -0400

    feat: simplify logging

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -5,23 +5,21 @@ import { promises as fs } from "fs";
 import picocolors from "picocolors";
 import { Formatter } from "picocolors/types";
 
-const { blue, red, gray } = picocolors;
+const { blue, red } = picocolors;
 
 export class Logger {
     public static logMessage(guildId: string, authorId: string, createdTimestamp: number): void {
         this.writeToFile(`timestamps/${guildId}/${authorId}`, createdTimestamp.toString());
     }
 
-    public static log(message: string, color: Formatter = blue, prefix: string = "log"): void {
-        const timestamp = getTimestamp();
-        const content = `[${prefix.toUpperCase()}] ${message}`;
-
-        console.log(`${gray(timestamp)} ${color(content)}`);
-        this.writeToFile("log", `${timestamp} ${content}`);
+    public static error(error: string, color: Formatter = red): void {
+        this.log(error, color);
     }
 
-    public static error(error: string, color: Formatter = red): void {
-        this.log(error, color, "Error");
+    public static log(message: string, color: Formatter = blue): void {
+        console.log(`${color(message)}`);
+        const timestamp = getTimestamp();
+        this.writeToFile("log", `${timestamp} ${message}`);
     }
 
     public static async writeToFile(filename: string, message: string): Promise<void> {

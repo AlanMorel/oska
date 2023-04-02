6e9ef52e1f02b723cc6dc98df49b3be69028b24a
Alan Morel <alan@alanmorel.com>
2023-04-02 16:40:08 -0400

    feat: overhaul logging

--- src/utils/Date.ts
+++ src/utils/Date.ts
@@ -12,3 +12,15 @@ const timestampOptions = {
 export function getTimestamp(date: Date = new Date()): string {
     return date.toLocaleDateString(Config.locale, timestampOptions);
 }
+
+export function formatTimestamp(date: string | Date): string {
+    const options: Intl.DateTimeFormatOptions = {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    };
+    return new Date(date).toLocaleDateString("en-US", options);
+}
--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,5 +1,4 @@
-import Config from "@/Config";
-import { getTimestamp } from "@/utils/Date";
+import { formatTimestamp } from "@/utils/Date";
 import { pathExists } from "@/utils/PathExists";
 import { promises as fs } from "fs";
 import picocolors from "picocolors";
@@ -9,7 +8,7 @@ const { blue, red } = picocolors;
 
 export class Logger {
     public static logMessage(guildId: string, authorId: string, createdTimestamp: number): void {
-        this.writeToFile(`timestamps/${guildId}/${authorId}`, createdTimestamp.toString());
+        this.write(`timestamps/${guildId}/`, `${authorId}`, createdTimestamp.toString());
     }
 
     public static error(error: string, color: Formatter = red): void {
@@ -18,12 +17,26 @@ export class Logger {
 
     public static log(message: string, color: Formatter = blue): void {
         console.log(`${color(message)}`);
-        const timestamp = getTimestamp();
-        this.writeToFile("log", `${timestamp} ${message}`);
+        this.writeToFile("log", message);
     }
 
-    public static async writeToFile(filename: string, message: string): Promise<void> {
-        const directory = `${Config.root}/logs`;
+    private static async writeToFile(type: string, message: string, id: string | null = null): Promise<void> {
+        const now = new Date();
+
+        const timestamp = now.toISOString().split("T")[0];
+        const [year, month] = [...timestamp.split("-")];
+
+        const timestampReadable = formatTimestamp(now);
+        const log = `[${timestampReadable}]: ${message}\r\n`;
+
+        await this.write(`logs/${type}/${year}/${month}`, timestamp, log);
+
+        if (id) {
+            await this.write(`logs/users/${id}/${type}/${year}/${month}`, timestamp, log);
+        }
+    }
+
+    private static async write(directory: string, filename: string, message: string): Promise<void> {
         const path = `${directory}/${filename}.log`;
 
         const exists = await pathExists(directory);
@@ -32,6 +45,6 @@ export class Logger {
             await fs.mkdir(directory, { recursive: true });
         }
 
-        await fs.appendFile(path, `${message}\r\n`);
+        await fs.appendFile(path, message);
     }
 }
